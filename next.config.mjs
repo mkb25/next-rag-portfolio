92dc69ad6c9fb11c228d3b0bd8bd701d68b33918
Mohana KB <mohanakrishna.bhat@costrategix.com>
2026-05-11 16:35:47 +0530

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const currentDirectory = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(currentDirectory, ".."),
   serverExternalPackages: ["pdf-parse"],
 };
 
